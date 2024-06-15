530c5c5e541c282a4bf87564949a974231a51b7a
Hitesh Yadav <hiteshy468@gmail.com>
2024-06-15 12:58:10 +0530

    make a mobile header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,43 +1,102 @@
-import React from 'react'
-import './Header.css'
-import { Link } from 'react-router-dom'
-import logo from './logo.png'
+import React, { useState } from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 function Header() {
+    const [isMenuActive, setIsMenuActive] = useState(false);
+    const [isHealthActive, setIsHealthActive] = useState(false);
+    const [isHealthMobActive, setIsHealthMobActive] = useState(false);
+
+    const handleMenuActive = () => {
+        setIsMenuActive(!isMenuActive);
+    };
+
+    const handleMenuDeActive = () => {
+        setIsMenuActive(false);
+    };
+
+    const handleHealthActive = () => {
+        setIsHealthActive(true);
+    };
+
+    const handleHealthDeActive = () => {
+        setIsHealthActive(false);
+    };
+
+    const handleHealthMobActive = () => {
+        setIsHealthMobActive(!isHealthMobActive);
+    };
+
     return (
         <header>
             <div className="container">
                 <div className="top">
                     <div className="content">
-                        <Link >My Account</Link>
-                        <Link >Contact Us</Link>
-                        <Link to={'/about'} >About Us</Link>
-                        <Link >Blogs</Link>
+                        <Link>My Account</Link>
+                        <Link>Contact Us</Link>
+                        <Link to={'/about'}>About Us</Link>
+                        <Link>Blogs</Link>
                     </div>
                 </div>
                 <div className="bottom">
-                    <Link to={'/'} className="logo">
-                        <img src={logo} alt="" />
+                    <Link onClick={handleMenuDeActive} to={'/'} className="logo">
+                        <img src={logo} alt="Logo" />
                     </Link>
                     <nav>
-                        <ul>
-                            <li><Link to={''} >Health Insurance</Link></li>
-                            <li><Link to={''} >Car Insurance</Link></li>
-                            <li><Link to={''} >Term Insurance</Link></li>
-                            <li><Link to={''} >Investment Plans</Link></li>
-                            <li><Link to={''} >Other Insurance</Link></li>
+                        <ul className='desktop-mod'>
+                            <li>
+                                <Link
+                                    to={''}
+                                    onMouseEnter={handleHealthActive}
+                                    onMouseLeave={handleHealthDeActive}
+                                    className="health-pointer"
+                                >
+                                    Health Insurance
+                                    <ul className={`health-hover ${isHealthActive ? 'health-active' : ''}`}>
+                                        <li><Link onClick={handleMenuDeActive} to={''}>Life Insurance</Link></li>
+                                        <li><Link onClick={handleMenuDeActive} to={''}>Life Insurance</Link></li>
+                                    </ul>
+                                </Link>
+                            </li>
+                            <li><Link to={''} onClick={handleMenuDeActive}>Car Insurance</Link></li>
+                            <li><Link to={''} onClick={handleMenuDeActive}>Term Insurance</Link></li>
+                            <li><Link to={''} onClick={handleMenuDeActive}>Investment Plans</Link></li>
+                            <li><Link to={''} onClick={handleMenuDeActive}>Other Insurance</Link></li>
+                        </ul>
+                        <ul className={`mob-mod ${isMenuActive ? 'menu-Active' : ''}`}>
+                            <li>
+                                <Link
+                                    className='health-mob-pointer'
+                                    onClick={handleHealthMobActive}
+                                >
+                                    Health Insurance
+                                </Link>
+                                <ul className={`health-mob-hover ${isHealthMobActive ? 'health-mob-active' : ''}`}>
+                                    <li><Link onClick={handleMenuDeActive}>Life Insurance</Link></li>
+                                    <li><Link onClick={handleMenuDeActive}>Life Insurance</Link></li>
+                                </ul>
+                            </li>
+                            <li><Link onClick={handleMenuDeActive}>Car Insurance</Link></li>
+                            <li><Link onClick={handleMenuDeActive}>Term Insurance</Link></li>
+                            <li><Link onClick={handleMenuDeActive}>Other Insurance</Link></li>
+                            <div className="social-link">
+                                <i className="ri-facebook-box-line"></i>
+                                <i className="ri-instagram-line"></i>
+                                <i className="ri-twitter-line"></i>
+                            </div>
                         </ul>
                     </nav>
                     <div className="btn-box">
-                        <Link>Apply Now</Link>
-                    <div className="menu">
-                        <i class="ri-menu-line"></i>
-                    </div>
+                        <Link onClick={handleMenuDeActive}>Apply Now</Link>
+                        <div className="menu" onClick={handleMenuActive}>
+                            <i className="ri-menu-line"></i>
+                        </div>
                     </div>
                 </div>
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
